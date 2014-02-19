19a1d4dfefb681c6dce7bd0755ecc06df42cbf5f
Andrew Yasinsky <andrew@yasinsky.com>
2014-02-18 19:28:10 -0800

    -updated cron

--- code/backup/bin/setupcron.sh
+++ code/backup/bin/setupcron.sh
@@ -3,7 +3,7 @@
 COMMAND="php /var/www/cronrat/artisan command:ratcheck --env=production"; 
 FILE="/tmp/$(basename $0).$RANDOM.txt"; 
 FILE2="/tmp/$(basename $0).$RANDOM.txt"; 
-ENTRY="5 * * * * $COMMAND"; 
+ENTRY="*/5 * * * * $COMMAND"; 
 crontab -l > $FILE 2> /dev/null; 
 cat $FILE | grep -v "$COMMAND" > $FILE2; 
 echo "$ENTRY" >> $FILE2; 
