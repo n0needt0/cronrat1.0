95fd8ce3f0580683b303b73e68822b8d949d10b5
Andrew Yasinsky <andrew@yasinsky.com>
2014-03-04 14:25:27 -0800

    -a

--- code/var/cronrat/app/views/cronrat/index.blade.php
+++ code/var/cronrat/app/views/cronrat/index.blade.php
@@ -40,8 +40,8 @@ $(document).on("pagecreate", function (e) {
 <div id='info' class="ui-body alert-info">
 @if(empty($rats))
 <h4>Where are my rats?</h4>
-<h4>click <a href=" {{Config::get('app.url')}}/r/{{$cronrat_code}}/MyTestRat"> {{Config::get('app.url')}}/r/coOSSWrq/TEST</a> to test</h4>
-<h4>then click back to this page</h4>
+<h4>click <a href=" {{Config::get('app.url')}}/r/{{$cronrat_code}}/MyTestRat" target="_new"> {{Config::get('app.url')}}/r/coOSSWrq/TEST</a> to test</h4>
+<h4>then refresh this page</h4>
 <h4>read FAQ on how to use Cronrat</h4>
 
 @endif
