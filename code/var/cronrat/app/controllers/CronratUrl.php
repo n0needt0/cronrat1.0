79bd96e6cea6aa589c95ff7c93b5991874267411
Andrew Yasinsky <andrew@yasinsky.com>
2014-03-28 13:00:29 -0700

    -fixed nactivity

--- code/var/cronrat/app/controllers/CronratUrl.php
+++ code/var/cronrat/app/controllers/CronratUrl.php
@@ -54,6 +54,52 @@ class CronratUrl extends BaseController {
             $toutc='0';
         }
 
+        $inactive = strpos($activeon,'0'); //see if any day s inactive
+        $daystoskip = 0;
+
+        if($inactive !== false)
+        {
+            //now lets see when this needs to run next. it be greater of $ttl (nextcheck) or next day to run on via active ON
+
+            $weekday = date('w',time() + intval($toutc));
+
+           //lets see if position
+            $pos = intval($weekday);
+
+            if(0 == $pos)
+            {
+                $pos=7; //sunday is 7, rest of wold has monday based weeks
+            }
+
+            //now see if next day is OFF
+            if('0' == substr($activeon, $pos-1, 1))
+            {
+                $daystoskip = 1;
+
+                while(true)
+                {
+                    //look for next working day
+
+                    $pos++;
+
+                    if(7 == $pos)
+                    {
+                        $pos=1; //reset to beginnig of the week
+                    }
+
+                    if('0' == substr($activeon, $pos-1, 1))
+                   {
+                      $daystoskip++;
+                   }
+                     else
+                   {
+                      break;
+                   }
+                }
+            }
+            $nextcheck = $nextcheck + (1440 * $daystoskip);
+        }
+
         try{
             $res = Rat::check_set_rat($ratkey, $ratname, $nextcheck, $emailto, $urlto, $activeon,$toutc);
             if($res)
